c84e10cd4d7ef3e7dd6520ec74b9f7d271ced6bc
Dmitriu <nihomas256@gmail.com>
2018-08-16 13:42:43 +0300

    Карта товара                                    Изменение src фото по выбору цвета.

---
+++ js/index.js
@@ -0,0 +1,27 @@
+
+
+$('#red').bind('click', function(){
+    $('img').attr({src: 'images/red.jpg', alt: 'red'});
+});
+
+$('#blue').bind('click', function(){
+    $('img').attr({src: 'images/blue.jpg', alt: 'blue'});
+});
+
+$('#green').bind('click', function(){
+    $('img').attr({src: 'images/green.jpg', alt: 'green'});
+});
+
+$('#yellow').bind('click', function(){
+    $('img').attr({src: 'images/yellow.jpg', alt: 'yellow'});
+});
+
+$('#black').bind('click', function(){
+    $('img').attr({src: 'images/black.jpg', alt: 'black'});
+});
+
+$('#white').bind('click', function(){
+    $('img').attr({src: 'images/white.jpg', alt: 'white'});
+});
+
+
