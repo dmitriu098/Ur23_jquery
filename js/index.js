c905f2533fde420aa80685faf31c302130ba8ae0
Dmitriu <nihomas256@gmail.com>
2018-08-16 13:46:32 +0300

    Карта товара                                    Изменение src фото по выбору цвета.

--- js/index.js
+++ js/index.js
@@ -1,27 +1,29 @@
+$(document).ready(function() {
 
+    $('#red').bind('click', function(){
+        $('img').attr({src: 'images/red.jpg', alt: 'red'});
+    });
 
-$('#red').bind('click', function(){
-    $('img').attr({src: 'images/red.jpg', alt: 'red'});
-});
+    $('#blue').bind('click', function(){
+        $('img').attr({src: 'images/blue.jpg', alt: 'blue'});
+    });
 
-$('#blue').bind('click', function(){
-    $('img').attr({src: 'images/blue.jpg', alt: 'blue'});
-});
+    $('#green').bind('click', function(){
+        $('img').attr({src: 'images/green.jpg', alt: 'green'});
+    });
 
-$('#green').bind('click', function(){
-    $('img').attr({src: 'images/green.jpg', alt: 'green'});
-});
+    $('#yellow').bind('click', function(){
+        $('img').attr({src: 'images/yellow.jpg', alt: 'yellow'});
+    });
 
-$('#yellow').bind('click', function(){
-    $('img').attr({src: 'images/yellow.jpg', alt: 'yellow'});
-});
+    $('#black').bind('click', function(){
+        $('img').attr({src: 'images/black.jpg', alt: 'black'});
+    });
 
-$('#black').bind('click', function(){
-    $('img').attr({src: 'images/black.jpg', alt: 'black'});
-});
+    $('#white').bind('click', function(){
+        $('img').attr({src: 'images/white.jpg', alt: 'white'});
+    });
 
-$('#white').bind('click', function(){
-    $('img').attr({src: 'images/white.jpg', alt: 'white'});
 });
 
 
